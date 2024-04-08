4a2d4ac8b3c278fb4a32593f37567e8b44fa944a
Rudy9025 <r8825899037@gmail.com>
2024-04-08 12:12:54 +0530

    initial commits

--- index.js
+++ index.js
@@ -1,102 +1,117 @@
-const { address } = require('address');
-const si = require('systeminformation');
-const express = require('express');
-const { User } = require('./model/mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-var cors = require('cors')
+const { address } = require("address");
+const express = require("express");
+const { User } = require("./model/mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const { UserInfo, KernelVersion, Hostname, MachineType, Architecture, AvailableParallelism, CpuCoresResult, Homedirectory, NetworkInterfaces, Platform, Release, Tmpdirectory, TotalMemory, FreeMemory, OsType, } = require("./model/os");
+const cors = require("cors");
 
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "index");
 });
 
 // Middleware to get IP address --------------------------------start------------------------------
 app.use(async (req, res, next) => {
-    try {
-        const addrs = await getAddress();
-        req.addrs = addrs;
-        next();
-    } catch (error) {
-        console.error('Error getting address:', error);
-        req.addrs = {};
-        next();
-    }
+  try {
+    const addrs = await getAddress();
+    req.addrs = addrs;
+    next();
+  } catch (error) {
+    console.error("Error getting address:", error);
+    req.addrs = {};
+    next();
+  }
 });
 
 async function getAddress() {
-    return new Promise((resolve, reject) => {
-        address((err, addrs) => {
-            if (err) {
-                reject(err);    
-            } else {
-                resolve(addrs);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    address((err, addrs) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(addrs);
+      }
     });
+  });
 }
 // Middleware to get IP address --------------------------------end------------------------------
 
-
-
 // Post data into database
-app.post('/userSignup', async (req, res) => {
-    const body = req.body;
-    try {
+app.post("/userSignup", async (req, res) => {
+  const body = req.body;
+  try {
     const result = await User.create({
-        first_name: body.first_name,
-        last_name: body.last_name,
-        email: body.email,
-        password: body.password,
-        ipv6_address: req.addrs.ipv6,
-        public_ipv4: req.addrs.ip,
-        MAC_address:req.addrs.mac,
-        
-    });  
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: body.password,
+      ipv6_address: req.addrs.ipv6,
+      public_ipv4: req.addrs.ip,
+      MAC_address: req.addrs.mac,
+      OS_NetworkDetails: {
+        username: UserInfo.username,
+        kernelVersion: KernelVersion,
+        hostname: Hostname,
+        machineType: MachineType,
+        architecture: Architecture,
+        availableParallelism: AvailableParallelism,
+        cpuCores: CpuCoresResult,
+        homedir: Homedirectory,
+        networkInterfaces: NetworkInterfaces,
+        platform: Platform,
+        release: Release,
+        tmpdir: Tmpdirectory,
+        totalMemory: TotalMemory,
+        freeMemory: FreeMemory,
+        osType: OsType,
+      },
+    });
     console.log(result);
-    console.log('Inserted successfully');
-    return res.redirect('/signup_success.html');
-} catch (error) {
-    console.error('Error inserting data into database:', error);
-    res.status(500).json('Internal Server Error');
-}
+    console.log("Inserted successfully");
+    return res.redirect("/signup_success.html");
+  } catch (error) {
+    console.error("Error inserting data into database:", error);
+    res.status(500).json("Internal Server Error");
+  }
 });
 
 // Login data
-app.post('/userLogin', async (req, res) => {
-    const { email, password } = req.body; 
-    if (!email || !password) {
-        return res.status(400).json({ error: 'Email and password are required.' });
-    }
-    try {
-        const result = await User.find({});
+app.post("/userLogin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required." });
+  }
+  try {
+    const result = await User.find({});
     const user = await User.findOne({ email, password });
 
-    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-        res.json(result); 
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      res.json(result);
     } else if (!user) {
-        res.sendFile(path.join(__dirname, 'public', 'invalid.html')); 
+      res.sendFile(path.join(__dirname, "public", "invalid.html"));
     } else {
-        res.sendFile(path.join(__dirname, 'public', 'Login_success.html')); 
+      res.sendFile(path.join(__dirname, "public", "Login_success.html"));
     }
-    } catch (error) {
-        console.error('Login error:', error);
-        return res.status(500).json({ error: 'Internal Server Error.' });
-    }
+  } catch (error) {
+    console.error("Login error:", error);
+    return res.status(500).json({ error: "Internal Server Error." });
+  }
 });
 
-
-
 app.listen(port, () => {
-    console.log(`Server started at port:${port}`);
+  console.log(`Server started at port:${port}`);
 });
--- model/mongoose.js
+++ model/mongoose.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
+
+//os_Network schema
+const OS_NetworkDetailsSchema = new mongoose.Schema({
+    username: String,
+    kernelVersion: String,
+    hostname: String,
+    machineType: String,
+    architecture: String,
+    availableParallelism: Number,
+    cpuCores: [{
+        model: String,
+        speed: String,
+        times: {
+            user: String,
+            nice: String,
+            sys: String,
+            idle: String,
+            irq: String,
+        }
+    }],
+    homedir: String,
+    networkInterfaces: mongoose.Schema.Types.Mixed, // Since networkInterfaces is an object with varying structure
+    platform: String,
+    release: String,
+    tmpdir: String,
+    totalMemory: Number,
+    freeMemory: Number,
+    osType: String
+});
+
+
 // Mongoose schema
 const userSchema = new mongoose.Schema({
     first_name: {
@@ -28,10 +59,11 @@ const userSchema = new mongoose.Schema({
     },
     MAC_address: {
         type: String,
-    },        
+    },   
+    OS_NetworkDetails:[OS_NetworkDetailsSchema]     
 }, { timestamps: true });
 
-
+ 
 // Mongoose model
 const User = mongoose.model('userDetails', userSchema);
 
